48f316e9573c2dcde8c554f94cf4c5b0979d13ec
Michael Trans <mich282q@edu.easj.dk>
2018-05-21 08:55:14 +0200

    Initial commit

---
+++ app/src/main/java/com/jfdimarzio/basicviews3/MainActivity.java
@@ -0,0 +1,34 @@
+package com.jfdimarzio.basicviews3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+    String[] presidents = {
+            "Dwight D. Eisenhower",
+            "John F. Kennedy",
+            "Lyndon B. Johnson",
+            "Richard Nixon",
+            "Gerald Ford",
+            "Jimmy Carter",
+            "Ronald Reagan",
+            "George H. W. Bush",
+            "Bill Clinton",
+            "George W. Bush",
+            "Barack Obama"
+    };
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, presidents);
+        AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.txtCountries);
+        textView.setThreshold(3);
+        textView.setAdapter(adapter);
+    }
+}
